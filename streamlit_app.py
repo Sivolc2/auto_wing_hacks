1c6116890d601a9837d41605be0bf63bf19e0a6b
sivolc <sivolc11@gmail.com>
2023-07-22 17:48:37 -0700

    bugfix

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,6 +25,14 @@ import os
 
 DB_PATH = (Path(__file__).parent / "Chinook.db").absolute()
 
+
+# New function to run agent with custom prompt
+def run_with_prompt(input, tools, llm, prompt):
+    llm.add_prompt(prompt)
+    result = llm.run(input, tools=tools)
+    llm.clear_prompt()
+    return result
+
 SAVED_SESSIONS = {
     "Who is Leo DiCaprio's girlfriend? What is her current age raised to the 0.43 power?": "leo.pickle",
     "What is the full name of the artist who recently released an album called "
@@ -43,17 +51,11 @@ openai_api_key = st.write(
 
 # Tools setup
 llm = OpenAI(temperature=0, openai_api_key=openai_api_key, streaming=True)
-# search = DuckDuckGoSearchAPIWrapper()
 llm_math_chain = LLMMathChain.from_llm(llm)
 db = SQLDatabase.from_uri(f"sqlite:///{DB_PATH}")
 db_chain = SQLDatabaseChain.from_llm(llm, db)
 search_google = GoogleSerperAPIWrapper()
 tools = [
-    # Tool(
-    #     name="Search",
-    #     func=search.run,
-    #     description="useful for when you need to answer questions about current events. You should ask targeted questions",
-    # ),
     Tool(
         name="Calculator",
         func=llm_math_chain.run,
@@ -62,31 +64,7 @@ tools = [
     Tool(
         name="FooBar DB",
         func=db_chain.run,
-        description="useful for whenrkl = initialize_agent(tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True) 
-
-tool_order_prompt = """
-The tools should be used in this order for answering questions:
-1. SearchG - Use this to search the internet for general information
-2. Calculator - Use this for any math calculations 
-3. FooBar DB - Use this to look up information in the FooBar database
-4. Mermaid - Use this to generate mermaid diagrams
-You should follow this order whenever possible when answering the user's questions.
-"""
-
-# New function to run agent with custom prompt
-def run_with_prompt(input, tools, llm, prompt):
-    llm.add_prompt(prompt)
-    result = llm.run(input, tools=tools)
-    llm.clear_prompt()
-    return result
-
-with tabs[0]:
-
-   # Existing code
-
-   answer = run_with_prompt(user_input, tools, llm, tool_order_prompt)
-
-   # Existing code you need to answer questions about FooBar. Input should be in the form of a question containing full context",
+        description="useful for when you need to answer questions about FooBar. Input should be in the form of a question containing full context",
     ),
     Tool(
        name="Mermaid",
@@ -100,21 +78,16 @@ with tabs[0]:
     )
 ]
 
-# Initialize agent
-mrkl = initialize_agent(tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True)
+mrkl = initialize_agent(tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True) 
 
 tool_order_prompt = """
 The tools should be used in this order for answering questions:
-1. Use your knowledge to create a list of what products go into the user-specified item (ie 'coffee' breaks down into 'coffee beans', 'milk', and 'sugar')
-2. SearchG - Use this to search the internet for potential products
+1. SearchG - Use this to search the internet for general information
+2. Calculator - Use this for any math calculations 
+3. FooBar DB - Use this to look up information in the FooBar database
+4. Mermaid - Use this to generate mermaid diagrams
 You should follow this order whenever possible when answering the user's questions.
 """
-
-def run_with_prompt(input, tools, llm, prompt):
-    llm.add_prompt(prompt)
-    result = llm.run(input, tools=tools)
-    llm.clear_prompt()
-    return result
 
 # Existing imports and setup
 tabs = st.tabs(["QA", "Product Search"])
@@ -126,7 +99,7 @@ with tabs[0]:
         #     user_input = prefilled
         submit_clicked = st.form_submit_button("Submit Inquiry")
         ## Add langchain preprompt?
-    
+
 
 with tabs[1]:
     products = st.text_input("Enter comma-separated list of products")
@@ -164,6 +137,9 @@ with tabs[1]:
                 mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
             )
 
+
+
+
 output_container = st.empty()
 if with_clear_container(submit_clicked):
     output_container = output_container.container()
@@ -174,9 +150,6 @@ if with_clear_container(submit_clicked):
 
     # If we've saved this question, play it back instead of actually running LangChain
     # (so that we don't exhaust our API calls unnecessarily)
-    # answer = mrkl.run(user_input, callbacks=[st_callback])
     answer = run_with_prompt(user_input, tools, mrkl, tool_order_prompt)
-
-    ## Run
 
     answer_container.write(answer)
